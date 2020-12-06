848c683ec66eb7f139d69744bb99e4f3ed809fd5
Fahmi Ihsan <fahmi.ihsan@student.umn.ac.id>
2020-12-06 13:26:19 +0700

    merubah app.js

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      secondsElapsed: 122 //time in seconds
+    };
+  }
+
+  getHours() {
+    return ("0" + Math.floor(this.state.secondsElapsed / 3600)).slice(-2);
+  }
+
+  getMinutes() {
+    return ("0" + Math.floor((this.state.secondsElapsed % 3600) / 60)).slice(-2);
+  }
+
+  getSeconds() {
+    return ("0" + (this.state.secondsElapsed % 60)).slice(-2);
+  }
+
+  startTime() {
+    var _this = this;
+    this.countdown = setInterval(function() {
+      _this.setState({ secondsElapsed: _this.state.secondsElapsed - 1 });
+    }, 1000);
+  }
+
+  resetTime() {
+    this.reset = this.setState({
+      secondsElapsed: (this.state.secondsElapsed = 0)
+    });
+  }
+
+  pauseTime() {
+    clearInterval(this.countdown);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.getHours()}:{this.getMinutes()}:{this.getSeconds()}
+        </h1>
+        <button onClick={() => this.startTime()}>Start</button>
+        <button onClick={() => this.pauseTime()}>Pause</button>
+        <button onClick={() => this.resetTime()}>Reset</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Timer;
